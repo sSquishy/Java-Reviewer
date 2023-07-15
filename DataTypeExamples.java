acad0d26f0da06519857051cc55432b0c3270cf0
sSquishy <johnpaulopbattung@iskolarngbayan.pup.edu.ph>
2023-07-15 23:23:51 +0800

    data types
    sample 01

---
+++ DataTypeExamples.java
@@ -0,0 +1,43 @@
+public class DataTypeExamples {
+    public static void main(String[] args) {
+        // Numeric Data Types
+        int age = 25;
+        System.out.println("Age is an integer data type: " + age);
+
+        double price = 19.99;
+        System.out.println("Price is a double data type: " + price);
+
+        float temperature = 28.5f;
+        System.out.println("Temperature is a float data type: " + temperature);
+
+        long population = 10000000000L;
+        System.out.println("Population is a long data type: " + population);
+
+        short quantity = 100;
+        System.out.println("Quantity is a short data type: " + quantity);
+
+        byte flags = 127;
+        System.out.println("Flags is a byte data type: " + flags);
+
+        // Character Data Type
+        char grade = 'A';
+        System.out.println("Grade is a character data type: " + grade);
+
+        // Boolean Data Type
+        boolean isActive = true;
+        System.out.println("isActive is a boolean data type: " + isActive);
+
+        // String Data Type
+        String name = "John Doe";
+        System.out.println("Name is a string data type: " + name);
+
+        // Array Data Type
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("Numbers is an array data type: " + numbers);
+
+        // Object Data Type
+        Object obj = new Object();
+        System.out.println("Obj is an object data type: " + obj);
+    }
+}
+
