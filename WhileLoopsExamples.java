1f651d9960df23041e5fe21212756d6c10839bc9
sSquishy <johnpaulopbattung@iskolarngbayan.pup.edu.ph>
2023-07-24 23:04:01 +0800

    .

--- WhileLoopsExamples.java
+++ WhileLoopsExamples.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class WhileLoopsExamples {
     public static void mainn(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        while (count <= 5) {
+            System.out.println("While loop iteration " + count);
+            count++;
+        }
+
+        System.out.println("---------------------------------");
 
     }
 
