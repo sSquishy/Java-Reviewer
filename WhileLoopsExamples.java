e8dfc5a84ae9e6e113babc2d4dbda37faba8f1ad
sSquishy <johnpaulopbattung@iskolarngbayan.pup.edu.ph>
2023-07-24 23:07:25 +0800

    .

--- WhileLoopsExamples.java
+++ WhileLoopsExamples.java
@@ -4,10 +4,27 @@ public class WhileLoopsExamples {
     public static void mainn(String[] args){
         Scanner scanner = new Scanner(System.in);
 
+        // While loop
+        int count = 1;
         while (count <= 5) {
             System.out.println("While loop iteration " + count);
             count++;
         }
+
+        System.out.println("---------------------------------");
+        
+        // While loop with user input
+        int sum = 0;
+        int number;
+        System.out.println("Enter numbers to find their sum (enter 0 to stop): ");
+        while (true) {
+            number = scanner.nextInt();
+            if (number == 0) {
+                break;
+            }
+            sum += number;
+        }
+        System.out.println("Sum: " + sum);
 
         System.out.println("---------------------------------");
 
