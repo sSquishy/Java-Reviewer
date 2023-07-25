b4b086de9ea7dde8278220e02158a4cb312c1b9f
sSquishy <johnpaulopbattung@iskolarngbayan.pup.edu.ph>
2023-07-25 15:41:51 +0800

    Nester Loop

--- ForLoopExamples.java
+++ ForLoopExamples.java
@@ -9,6 +9,16 @@ class ForLoopExamples{
             System.out.println(i);
           }
 
+        //Nested Loop
+            // Outer loop
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("Outer: " + i); // Executes 2 times
+            
+            // Inner loop
+            for (int j = 1; j <= 3; j++) {
+            System.out.println(" Inner: " + j); // Executes 6 times (2 * 3)
+            }
+        } 
     }
 
 }
