2305dcddca1d8d6cc564dd06e694b9f509f52c38
sSquishy <johnpaulopbattung@iskolarngbayan.pup.edu.ph>
2023-07-25 15:38:57 +0800

    For loop

--- ForLoopExamples.java
+++ ForLoopExamples.java
@@ -1,9 +1,13 @@
-import scanner;
+import java.util.Scanner;
 
 class ForLoopExamples{
     public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);
 
-
+        //For loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+          }
 
     }
 
