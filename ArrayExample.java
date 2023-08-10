89c186feac6ac1dc88def1adc51a90ca45142f80
sSquishy <johnpaulopbattung@iskolarngbayan.pup.edu.ph>
2023-08-10 20:06:55 +0800

    .

--- ArrayExample.java
+++ ArrayExample.java
@@ -3,5 +3,9 @@ public class ArrayExample {
     public static void main(String[] args) {
         // Example 1: Creating and Initializing an Array
         int[] numbers = {5, 2, 9, 1, 5};
+        System.out.println("Example 1: Creating and Initializing an Array");
+        System.out.println("Numbers array: " + Arrays.toString(numbers));
+
+
     }
 }
