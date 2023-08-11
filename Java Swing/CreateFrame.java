5104f54d32a411b93df1913235279a3cfe3000b4
sSquishy <johnpaulopbattung@iskolarngbayan.pup.edu.ph>
2023-08-11 23:47:16 +0800

    .

---
+++ Java Swing/CreateFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+class CreateFrame{
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setSize(550,500);
+        frame.setTitle("Create JFrame Example");
+        frame.setVisible(true);
+    }
+}
---
+++ Java Swing/Sample.java
@@ -0,0 +1,5 @@
+class Sample{
+    public static void main(String[] args) {
+        System.out.print("Hello WOrld");
+    }
+}
